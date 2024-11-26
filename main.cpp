3cc645214cd2914d6fb01e8850e479ed9e5c7063
Alex Martin <sharkfinpro@hotmail.com>
2024-11-26 13:29:13 -0800

    Load VulkanRenderer with CMake

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
+#include <VulkanEngine/VulkanEngine.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  try
+  {
+    constexpr VulkanEngineOptions vulkanEngineOptions = {
+      .WINDOW_WIDTH = 800,
+      .WINDOW_HEIGHT = 600,
+      .WINDOW_TITLE = "ECS3D"
+    };
+
+    VulkanEngine renderer(vulkanEngineOptions);
+
+    while (renderer.isActive())
+    {
+      renderer.render();
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
