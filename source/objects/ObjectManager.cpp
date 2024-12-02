8790dc2537b4a7e3b71ca48212587c0f7ba2981a
Alex Martin <sharkfinpro@hotmail.com>
2024-12-02 11:11:20 -0800

    add some skeleton to ObjectManager

--- source/objects/ObjectManager.cpp
+++ source/objects/ObjectManager.cpp
@@ -1,1 +1,19 @@
 #include "ObjectManager.h"
+
+ObjectManager::ObjectManager()
+{
+}
+
+void ObjectManager::update(const float dt)
+{
+  fixedUpdate(dt);
+  variableUpdate(dt);
+}
+
+void ObjectManager::variableUpdate(const float dt)
+{
+}
+
+void ObjectManager::fixedUpdate(const float dt)
+{
+}
--- source/objects/ObjectManager.h
+++ source/objects/ObjectManager.h
@@ -1,10 +1,22 @@
 #ifndef OBJECTMANAGER_H
 #define OBJECTMANAGER_H
 
+#include <vector>
+#include <memory>
 
+class Object;
 
 class ObjectManager {
+public:
+  ObjectManager();
 
+  void update(float dt);
+
+private:
+  std::vector<std::shared_ptr<Object>> objects;
+
+  void variableUpdate(float dt);
+  void fixedUpdate(float dt);
 };
 
 
