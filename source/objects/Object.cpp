44c2c9af0b07787a41043953ff59f0fc25ff548c
Alex Martin <sharkfinpro@hotmail.com>
2024-12-02 11:27:08 -0800

    add skeleton to Object

--- source/objects/Object.cpp
+++ source/objects/Object.cpp
@@ -1,1 +1,50 @@
 #include "Object.h"
+
+Object::Object(const std::vector<std::shared_ptr<Component>>& components)
+{
+  for (const auto& component : components)
+  {
+    addComponent(component);
+  }
+}
+
+void Object::addComponent(std::shared_ptr<Component> component)
+{
+  // TODO: Insert component into components
+}
+
+std::shared_ptr<Component> Object::getComponent(const ComponentType type) const
+{
+  if (const auto component = components.find(type); component != components.end())
+  {
+    return component->second;
+  }
+
+  return nullptr;
+}
+
+void Object::variableUpdate(const float dt)
+{
+  for (const auto& component : components)
+  {
+    // TODO: Execute component variableUpdate
+  }
+}
+
+void Object::fixedUpdate(const float dt)
+{
+  for (const auto& component : components)
+  {
+    // TODO: Execute component fixedUpdate
+  }
+}
+
+void Object::setManager(ObjectManager* objectManager)
+{
+  manager = objectManager;
+}
+
+ObjectManager* Object::getManager() const
+{
+  return manager;
+}
--- source/objects/Object.h
+++ source/objects/Object.h
@@ -1,10 +1,31 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include <vector>
+#include <unordered_map>
+#include <memory>
 
+class ObjectManager;
+
+enum class ComponentType;
+class Component;
 
 class Object {
+public:
+  explicit Object(const std::vector<std::shared_ptr<Component>>& components);
 
+  void addComponent(std::shared_ptr<Component> component);
+  std::shared_ptr<Component> getComponent(ComponentType type) const;
+
+  void variableUpdate(float dt);
+  void fixedUpdate(float dt);
+
+  void setManager(ObjectManager* objectManager);
+  ObjectManager* getManager() const;
+
+private:
+  std::unordered_map<ComponentType, std::shared_ptr<Component>> components;
+  ObjectManager* manager;
 };
 
 
