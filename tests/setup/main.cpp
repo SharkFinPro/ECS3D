fe366da0a47a40993b4f8fb449358b46b983ceed
Alex Martin <sharkfinpro@hotmail.com>
2024-12-03 12:05:49 -0800

    Update main.cpp

--- tests/setup/main.cpp
+++ tests/setup/main.cpp
@@ -5,24 +5,20 @@
 #include "source/objects/ObjectManager.h"
 #include "source/objects/components/Components.h"
 
+std::shared_ptr<Object> createBlock(const ECS3D& ecs, glm::vec3 position = { 0, 0, 0 });
+std::shared_ptr<Object> createRigidBlock(const ECS3D& ecs, glm::vec3 position = { 0, 0, 0 });
+
 int main()
 {
   try
   {
     ECS3D ecs;
 
-    const std::vector<std::shared_ptr<Component>> components {
-      std::make_shared<Transform>(glm::vec3{ 0, 0, 0 }),
-      std::make_shared<ModelRenderer>(ecs.getRenderer(),
-                                      "assets/textures/white.png",
-                                      "assets/textures/blank_specular.png",
-                                      "assets/models/cube_1x1x1.glb"),
-      std::make_shared<RigidBody>()
-    };
-
-    const auto object = std::make_shared<Object>(components);
-
+    const auto object = createRigidBlock(ecs);
     ecs.getObjectManager()->addObject(object);
+
+    const auto object2 = createBlock(ecs);
+    ecs.getObjectManager()->addObject(object2);
 
     while (ecs.isActive())
     {
@@ -36,4 +32,31 @@ int main()
   }
 
   return EXIT_SUCCESS;
+}
+
+std::shared_ptr<Object> createBlock(const ECS3D& ecs, glm::vec3 position)
+{
+  const std::vector<std::shared_ptr<Component>> components {
+    std::make_shared<Transform>(position),
+    std::make_shared<ModelRenderer>(ecs.getRenderer(),
+                                    "assets/textures/white.png",
+                                    "assets/textures/blank_specular.png",
+                                    "assets/models/cube_1x1x1.glb"),
+    std::make_shared<RigidBody>()
+  };
+
+  return std::make_shared<Object>(components);
+}
+
+std::shared_ptr<Object> createRigidBlock(const ECS3D& ecs, glm::vec3 position)
+{
+  const std::vector<std::shared_ptr<Component>> components {
+    std::make_shared<Transform>(position),
+    std::make_shared<ModelRenderer>(ecs.getRenderer(),
+                                    "assets/textures/white.png",
+                                    "assets/textures/blank_specular.png",
+                                    "assets/models/cube_1x1x1.glb")
+  };
+
+  return std::make_shared<Object>(components);
 }
