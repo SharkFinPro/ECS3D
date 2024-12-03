764d304b2aa66c0c8d7698e632b5082c9cc69fed
Alex Martin <sharkfinpro@hotmail.com>
2024-12-03 14:37:49 -0800

    create basic BoxCollider

---
+++ source/objects/components/collisions/BoxCollider.h
@@ -0,0 +1,26 @@
+#ifndef BOXCOLLIDER_H
+#define BOXCOLLIDER_H
+
+#include "Collider.h"
+#include <glm/vec3.hpp>
+#include <array>
+
+constexpr std::array<glm::vec3, 8> boxVertices = {{
+  {1.0f, 1.0f, 1.0f},
+  {-1.0f, 1.0f, 1.0f},
+  {-1.0f, 1.0f, -1.0f},
+  {1.0f, 1.0f, -1.0f},
+  {1.0f, 0.0f, 1.0f},
+  {-1.0f, 0.0f, 1.0f},
+  {-1.0f, 0.0f, -1.0f},
+  {1.0f, 0.0f, -1.0f}
+}};
+
+class BoxCollider final : public Collider {
+private:
+  glm::vec3 findFurthestPoint(const glm::vec3& direction) override;
+};
+
+
+
+#endif //BOXCOLLIDER_H
