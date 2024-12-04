acec8850a03e539ae43d299c91018f993b45c7f4
Alex Martin <sharkfinpro@hotmail.com>
2024-12-03 22:54:57 -0800

    Update BoxCollider.cpp

--- source/objects/components/collisions/BoxCollider.cpp
+++ source/objects/components/collisions/BoxCollider.cpp
@@ -3,6 +3,7 @@
 #include "../Transform.h"
 #include <stdexcept>
 #include <glm/glm.hpp>
+#include <limits>
 
 glm::vec3 BoxCollider::findFurthestPoint(const glm::vec3& direction)
 {
@@ -16,23 +17,21 @@ glm::vec3 BoxCollider::findFurthestPoint(const glm::vec3& direction)
     }
   }
 
-  float furthestDistance = -FLT_MAX;
+  float furthestDistance = std::numeric_limits<float>::lowest();
   glm::vec3 furthestVertex{ 0, 0, 0 };
 
   if (const std::shared_ptr<Transform> transform = transform_ptr.lock())
   {
-    std::array<glm::vec3, 8> mesh{};
-    for (int i = 0; i < 8; i++)
-    {
-      mesh[i] = boxVertices[i] * transform->getScale() + transform->getPosition();
-    }
+    const auto scale = transform->getScale();
+    const auto position = transform->getPosition();
 
-    for (auto& vertex : mesh)
+    for (auto& vertex : boxVertices)
     {
-      if (const float distance = dot(vertex, direction); distance > furthestDistance)
+      const glm::vec3 transformedVertex = vertex * scale + position;
+      if (const float distance = dot(transformedVertex, direction); distance > furthestDistance)
       {
         furthestDistance = distance;
-        furthestVertex = vertex;
+        furthestVertex = transformedVertex;
       }
     }
   }
