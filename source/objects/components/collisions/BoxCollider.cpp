6eff95236d2885bdfd1c019d75da4a3d441de80a
Alex Martin <sharkfinpro@hotmail.com>
2025-01-02 23:21:20 -0800

    cache transformed mesh in boxCollider

--- source/objects/components/collisions/BoxCollider.cpp
+++ source/objects/components/collisions/BoxCollider.cpp
@@ -7,7 +7,7 @@
 #include <limits>
 
 BoxCollider::BoxCollider()
-  : Collider(ColliderType::boxCollider, ComponentType::SubComponentType_boxCollider)
+  : Collider(ColliderType::boxCollider, ComponentType::SubComponentType_boxCollider), currentTransformUpdateID(255)
 {}
 
 void BoxCollider::displayGui()
@@ -28,33 +28,47 @@ glm::vec3 BoxCollider::findFurthestPoint(const glm::vec3& direction)
     }
   }
 
+  if (const std::shared_ptr<Transform> transform = transform_ptr.lock())
+  {
+    if (currentTransformUpdateID != transform->getUpdateID())
+    {
+      generateTransformedMesh(transform);
+    }
+  }
+
   float furthestDistance = std::numeric_limits<float>::lowest();
   glm::vec3 furthestVertex{ 0, 0, 0 };
 
-  if (const std::shared_ptr<Transform> transform = transform_ptr.lock())
+  for (auto& vertex : transformedBoxVertices)
   {
-    const auto rotation = transform->getRotation();
-    const auto scale = transform->getScale();
-    const auto position = transform->getPosition();
-
-    const auto transformationMatrix = translate(glm::mat4(1.0f), position)
-      * rotate(glm::mat4(1.0f), glm::radians(rotation.z), {0, 0, 1})
-      * rotate(glm::mat4(1.0f), glm::radians(rotation.y), {0, 1, 0})
-      * rotate(glm::mat4(1.0f), glm::radians(rotation.x), {1, 0, 0})
-      * glm::scale(glm::mat4(1.0f), scale);
-
-    for (auto& vertex : boxVertices)
+    if (const float distance = dot(vertex, direction); distance > furthestDistance)
     {
-      const auto transformedVertex4 = transformationMatrix * glm::vec4(vertex, 1.0f);
-      const auto transformedVertex = glm::vec3(transformedVertex4);
-
-      if (const float distance = dot(transformedVertex, direction); distance > furthestDistance)
-      {
-        furthestDistance = distance;
-        furthestVertex = transformedVertex;
-      }
+      furthestDistance = distance;
+      furthestVertex = vertex;
     }
   }
 
   return furthestVertex;
+}
+
+void BoxCollider::generateTransformedMesh(const std::shared_ptr<Transform>& transform)
+{
+  const auto rotation = transform->getRotation();
+  const auto scale = transform->getScale();
+  const auto position = transform->getPosition();
+
+  const auto transformationMatrix = translate(glm::mat4(1.0f), position)
+    * rotate(glm::mat4(1.0f), glm::radians(rotation.z), {0, 0, 1})
+    * rotate(glm::mat4(1.0f), glm::radians(rotation.y), {0, 1, 0})
+    * rotate(glm::mat4(1.0f), glm::radians(rotation.x), {1, 0, 0})
+    * glm::scale(glm::mat4(1.0f), scale);
+
+  for (size_t i = 0; i < boxVertices.size(); ++i)
+  {
+    const auto transformedVertex = transformationMatrix * glm::vec4(boxVertices[i], 1.0f);
+
+    transformedBoxVertices[i] = glm::vec3(transformedVertex);
+  }
+
+  currentTransformUpdateID = transform->getUpdateID();
 }
