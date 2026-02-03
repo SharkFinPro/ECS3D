4d728af760be7cbdb68d6042e2ca20b090131e13
Alex Martin <sharkfinpro@hotmail.com>
2026-02-02 23:36:37 -0800

    Render sphere collider in variableUpdate
    Add visualization for SphereCollider by implementing variableUpdate and header changes. The update lazily loads a sphere model (assets/models/sphere_3.glb) and texture (assets/textures/white.png) via the asset manager, creates a RenderObject, and updates its position, scale (transform scale * radius) and orientation from the Transform component. The object is rendered using the objectHighlight pipeline. Also add m_renderObject and m_renderCollider members and declare variableUpdate in the header, plus related include additions required for rendering and assets.

--- source/objects/components/collisions/SphereCollider.cpp
+++ source/objects/components/collisions/SphereCollider.cpp
@@ -1,8 +1,16 @@
 #include "SphereCollider.h"
 #include "../Transform.h"
 #include "../../Object.h"
+#include "../../../ECS3D.h"
+#include "../../../assets/AssetManager.h"
+#include "../../../assets/ModelAsset.h"
+#include "../../../assets/TextureAsset.h"
 #include <glm/gtx/component_wise.inl>
 #include <nlohmann/json.hpp>
+#include <VulkanEngine/components/assets/AssetManager.h>
+#include <VulkanEngine/components/pipelines/implementations/common/PipelineTypes.h>
+#include <VulkanEngine/components/renderingManager/RenderingManager.h>
+#include <VulkanEngine/components/renderingManager/renderer3D/Renderer3D.h>
 #include <stdexcept>
 
 SphereCollider::SphereCollider()
@@ -50,6 +58,49 @@ nlohmann::json SphereCollider::serialize()
 void SphereCollider::loadFromJSON(const nlohmann::json& componentData)
 {
   m_radius = componentData.at("radius");
+}
+
+void SphereCollider::variableUpdate([[maybe_unused]] const float dt)
+{
+  if (!m_renderCollider)
+  {
+    return;
+  }
+
+  if (m_transform_ptr.expired())
+  {
+    m_transform_ptr = m_owner->getComponent<Transform>(ComponentType::transform);
+
+    if (m_transform_ptr.expired())
+    {
+      return;
+    }
+  }
+
+  const auto renderer = m_owner->getManager()->getECS()->getRenderer();
+
+  if (!m_renderObject)
+  {
+    const auto assetManager = m_owner->getManager()->getECS()->getAssetManager();
+
+    const auto model = assetManager->getAsset<ModelAsset>("assets/models/sphere_3.glb");
+    const auto texture = assetManager->getAsset<TextureAsset>("assets/textures/white.png");
+
+    m_renderObject = renderer->getAssetManager()->loadRenderObject(texture->getTexture(), texture->getTexture(), model->getModel());
+  }
+
+  if (const std::shared_ptr<Transform> transform = m_transform_ptr.lock())
+  {
+    m_renderObject->setPosition(transform->getPosition());
+    m_renderObject->setScale(transform->getScale() * m_radius);
+    m_renderObject->setOrientationEuler(transform->getRotation());
+  }
+
+  renderer->getRenderingManager()->getRenderer3D()->renderObject(
+    m_renderObject,
+    vke::PipelineType::objectHighlight,
+    nullptr
+  );
 }
 
 glm::vec3 SphereCollider::findFurthestPoint(const glm::vec3& direction)
