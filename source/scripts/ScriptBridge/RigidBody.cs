7f83912325051980cf8255771b3c489a1fbd1957
Alex Martin <sharkfinpro@hotmail.com>
2026-03-16 13:46:26 -0700

    Embed binding structs into RigidBody/Transform
    Move the native interop binding structs into their respective class files and remove the separate *Bindings.cs files. Adds [StructLayout(LayoutKind.Sequential)] unsafe structs with unmanaged function-pointer fields to RigidBody.cs and Transform.cs (get/set/move/start/stop/applyForce/setVelocity, etc.). This colocates the native bindings with the related classes to reduce file churn and improve discoverability without changing the bindings' layout or signatures.

--- source/scripts/ScriptBridge/RigidBody.cs
+++ source/scripts/ScriptBridge/RigidBody.cs
@@ -3,6 +3,13 @@ using System.Runtime.InteropServices;
 
 namespace ScriptBridge;
 
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct RigidBodyBindings
+{
+    public delegate* unmanaged<IntPtr, float, float, float, float, float, float, void> applyForce;
+    public delegate* unmanaged<IntPtr, float, float, float, void> setVelocity;
+}
+
 public unsafe class RigidBody
 {
     private readonly IntPtr _uuid;
--- source/scripts/ScriptBridge/Transform.cs
+++ source/scripts/ScriptBridge/Transform.cs
@@ -4,6 +4,19 @@ using System.Runtime.InteropServices;
 
 namespace ScriptBridge;
 
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct TransformBindings
+{
+    public delegate* unmanaged<IntPtr, float*, float*, float*, void> getPosition;
+    public delegate* unmanaged<IntPtr, float*, float*, float*, void> getScale;
+    public delegate* unmanaged<IntPtr, float*, float*, float*, void> getRotation;
+    public delegate* unmanaged<IntPtr, float, float, float, void> setScale;
+    public delegate* unmanaged<IntPtr, float, float, float, void> setRotation;
+    public delegate* unmanaged<IntPtr, float, float, float, void> move;
+    public delegate* unmanaged<IntPtr, void> start;
+    public delegate* unmanaged<IntPtr, void> stop;
+}
+
 public unsafe class Transform
 {
     private readonly IntPtr _uuid;
